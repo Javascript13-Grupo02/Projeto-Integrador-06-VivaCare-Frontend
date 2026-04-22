29e7f7edf200055b3660aec9aa724fcae5e42c28
paula-alcantara <paullety.m@gmail.com>
2026-04-22 14:19:15 -0300

    ✨ New Feature: Login Funcionando

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import { BrowserRouter } from "react-router-dom";
+import Login from "./pages/login/Login";
+import { AuthProvider } from "./contexts/AuthContext";
 
 function App() {
-
   return (
-    <>
-    </>
+    <BrowserRouter>
+    <AuthProvider>
+      <Login />
+    </AuthProvider>
+    </BrowserRouter>
   )
 }
 
