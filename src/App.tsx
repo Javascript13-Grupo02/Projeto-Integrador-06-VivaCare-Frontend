85ad065bf8cb14c3c19b61c53618496c151d6c95
Daniel Nascimento <d.nasciimento@live.com>
2026-04-22 11:02:32 -0300

    ✨ New Feature: Criaçao da pagina Sobre.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Sobre from "./pages/sobre/Sobre";
+import Equipe from "./pages/equipe/Equipe";
 
 function App() {
-
   return (
-    <>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Sobre />} />
+        <Route path="/Equipe" element={<Equipe />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
