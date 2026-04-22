1bc245ac8d58e74eee7067f111b6bd83b65ef3c6
paula-alcantara <paullety.m@gmail.com>
2026-04-22 15:07:57 -0300

    ⚡ General Update: Navbar condicional e remoção do FormContato repetido

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,75 @@
+import { useContext, type ReactNode } from "react";
+import { Link, useNavigate } from "react-router-dom"
+//import { AuthContext } from "../../contexts/AuthContext";
+//import { ToastAlerta } from "../../utils/ToastAlerta";
+
+function Navbar() {
+
+    const navigate = useNavigate();
+
+    //const { usuario, handleLogout } = useContext(AuthContext)
+
+    function logout() {
+        //handleLogout()
+        //ToastAlerta('O Usuário foi desconectado com sucesso!', 'sucesso')
+        navigate('/')
+    }
+
+    let component: ReactNode
+    let componentLogin: ReactNode
+    
+    component = ( 
+        <div className='w-full flex justify-center py-4 bg-sky-800 text-white'>
+            <div className="container flex justify-between text-lg mx-8">
+                <Link to='/home' className="text-2xl font-bold">
+
+                    <img
+                        src=""
+                        alt=""
+                       // className="h-9 w-auto"
+                    />
+                    VivaCare
+                </Link>
+
+                <div className='flex gap-4'>
+                    <Link to='/sobre' className='hover:text-[#45a8f0] transition-colors'>Sobre</Link>
+                    <Link to='/equipe' className='hover:text-[#45a8f0] transition-colors'>Equipe</Link>
+                    <Link to='/login' className='hover:text-[#45a8f0] transition-colors'>Entrar</Link>
+                </div>
+            </div>
+        </div>
+    )
+    
+
+    componentLogin = ( 
+        <div className='w-full flex justify-center py-4 bg-sky-800 text-white'>
+            <div className="container flex justify-between text-lg mx-8">
+                <Link to='/home' className="text-2xl font-bold">
+
+                    <img
+                        src=""
+                        alt=""
+                       // className="h-9 w-auto"
+                    />
+                    VivaCare
+                </Link>
+
+                <div className='flex gap-4'>
+                    <Link to='/apolice' className='hover:text-[#45a8f0] transition-colors'>Apólice</Link>
+                    <Link to='/cliente' className='hover:text-[#45a8f0] transition-colors'>Cliente</Link>
+                    <Link to='/sobre' className='hover:text-[#45a8f0] transition-colors'>Sobre</Link>
+                    <Link to='/equipe' className='hover:text-[#45a8f0] transition-colors'>Equipe</Link>
+                    <Link to='' onClick={logout} className='hover:text-[#45a8f0] transition-colors'>Sair</Link>
+                </div>
+            </div>
+        </div>
+    )
+
+    return (
+        <>
+            {component}
+        </>
+    )
+}
+
+export default Navbar
