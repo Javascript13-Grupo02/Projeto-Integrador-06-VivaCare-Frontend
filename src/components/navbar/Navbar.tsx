3abea1b1fdad22d252d1d6ac109fbd95d2b1243c
paula-alcantara <paullety.m@gmail.com>
2026-04-24 14:51:27 -0300

    📱 Design: Navbar Esconder Apólices e Clientes

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -22,7 +22,7 @@ function Navbar() {
         <div className='w-full flex justify-center py-4 bg-linear-to-r from-sky-950 via-sky-900 to-sky-800 text-white'>
             <div className="container flex justify-between text-base sm:text-lg mx-8">
                 <Link to='/home' className="text-xl sm:text-2xl font-bold">
-                    <div className="flex gap-1 sm:gap-2">
+                    <div className="flex gap-0.5 sm:gap-2">
                     <img
                         src="https://ik.imagekit.io/vjqejp2vh/VivaCare/Logo%20VivaCare%20Clara.png?updatedAt=1777032061555"
                         alt=""
@@ -32,7 +32,7 @@ function Navbar() {
                     </div>
                 </Link>
 
-                <div className='flex gap-2 sm:gap-4'>
+                <div className='flex gap-1 sm:gap-4'>
                     <Link to='/sobre' className='hover:text-[#45a8f0] transition-colors'>Sobre</Link>
                     <Link to='/equipe' className='hover:text-[#45a8f0] transition-colors'>Equipe</Link>
                     <Link to='/login' className='hover:text-[#45a8f0] transition-colors'>Entrar</Link>
@@ -46,7 +46,7 @@ function Navbar() {
         <div className='w-full flex justify-center py-4 bg-linear-to-r from-sky-950 via-sky-900 to-sky-800 text-white'>
             <div className="container flex justify-between text-lg mx-8">
                 <Link to='/home' className="text-2xl font-bold">
-                    <div className="flex gap-1 sm:gap-2">
+                    <div className="flex gap-0.5 sm:gap-2">
                     <img
                         src="https://ik.imagekit.io/vjqejp2vh/VivaCare/Logo%20VivaCare%20Clara.png?updatedAt=1777032061555"
                         alt=""
@@ -56,9 +56,9 @@ function Navbar() {
                     </div>
                 </Link>
 
-                <div className='flex gap-2 sm:gap-4'>
-                    <Link to='/apolices' className='hover:text-[#45a8f0] transition-colors'>Apólices</Link>
-                    <Link to='/clientes' className='hover:text-[#45a8f0] transition-colors'>Clientes</Link>
+                <div className='flex gap-1 sm:gap-4'>
+                    <Link to='/apolices' className='hover:text-[#45a8f0] transition-colors hidden sm:flex gap-4'>Apólices</Link>
+                    <Link to='/clientes' className='hover:text-[#45a8f0] transition-colors hidden sm:flex gap-4'>Clientes</Link>
                     <Link to='/sobre' className='hover:text-[#45a8f0] transition-colors'>Sobre</Link>
                     <Link to='/equipe' className='hover:text-[#45a8f0] transition-colors'>Equipe</Link>
                     <Link to='/perfil' className='hover:text-[#45a8f0] transition-colors'>Perfil</Link>
